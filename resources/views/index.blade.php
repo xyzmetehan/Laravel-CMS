35cef0ec64aca839273b6452f5bfef667c9ba5f3
Metehan Özdemir <xyzmetehan@gmail.com>
2023-01-19 03:16:11 +0300

    Ana route düzenlendi ve temel blade dosyaları oluşturuldu.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title')
+    Anasayfa
+@endsection
+
+@section('css')
+@endsection
+
+@section('content')
+    <h1>Laravel CMS</h1>
+@endsection
+
+@section('js')
+@endsection
