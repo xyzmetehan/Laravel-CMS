82e1be188d37eaa6443215daeeeec2a0127527e9
Metehan Özdemir <xyzmetehan@gmail.com>
2023-01-19 19:37:16 +0300

    New theme applied. Routes has been modified.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,41 +6,48 @@
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>@yield('title')</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('assets/favicon.ico') }}"/>
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/favicon.ico') }}" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
-    @yield('css')
 </head>
-<body>
-<!-- Navigation Bar-->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <a class="navbar-brand" href="#!">Laravel CMS</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item"><a class="nav-link" href="#">Anasayfa</a></li>
-                <li class="nav-item"><a class="nav-link" href="#!">Hakkımızda</a></li>
-                <li class="nav-item"><a class="nav-link" href="#!">İletişim</a></li>
-            </ul>
+<body class="d-flex flex-column h-100">
+<main class="flex-shrink-0">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container px-5">
+            <a class="navbar-brand" href="{{ route('home.index') }}">Laravel CMS | @yield('title')</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="{{ route('home.index') }}">Anasayfa</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('home.about') }}">Hakkımızda</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('home.contact') }}">İletişim</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('home.pricing') }}">Fiyatlandırma</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('home.faq') }}">S.S.S.</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('home.blog') }}">Blog</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('home.portfolio') }}">Portfolyo</a></li>
+                </ul>
+            </div>
         </div>
-    </div>
-</nav>
-<!-- Page Header -->
-<header class="py-5 bg-light border-bottom mb-4">
-    <div class="container">
-        <div class="text-center my-5">
-            <h1 class="fw-bolder">Welcome to Blog Home!</h1>
-            <p class="lead mb-0">A Bootstrap 5 starter layout for your next blog homepage</p>
-        </div>
-    </div>
-</header>
-<!-- Page content-->
+    </nav>
 @yield('content')
+</main>
 <!-- Footer-->
-<footer class="py-5 bg-dark">
-    <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Laravel CMS by <a href="https://metehan.org">Metehan</a>
-        </p>
+<footer class="bg-dark py-4 mt-auto">
+    <div class="container px-5">
+        <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+            <div class="col-auto">
+                <div class="small m-0 text-white">Copyright &copy; Laravel CMS | Made by <a
+                            href="https://metehan.org">Metehan</a>
+                </div>
+            </div>
+            <div class="col-auto">
+                <a class="link-light small" href="#!">Gizlilik Politikası</a>
+                <span class="text-white mx-1">&middot;</span>
+                <a class="link-light small" href="#!">Şartlar & Koşullar</a>
+                <span class="text-white mx-1">&middot;</span>
+                <a class="link-light small" href="#!">İletişim</a>
+            </div>
+        </div>
     </div>
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', static function () {
+    return view('front.index');
+})->name('home.index');
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/about', static function () {
+    return view('front.about');
+})->name('home.about');
+
+Route::get('/contact', static function () {
+    return view('front.contact');
+})->name('home.contact');
+
+Route::get('/pricing', static function () {
+    return view('front.pricing');
+})->name('home.pricing');
+
+Route::get('/faq', static function () {
+    return view('front.faq');
+})->name('home.faq');
+
+Route::get('/blog', static function () {
+    return view('front.blog');
+})->name('home.blog');
+
+Route::get('/portfolio', static function () {
+    return view('front.portfolio');
+})->name('home.portfolio');
